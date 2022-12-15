ac2771c6eb134dd346d851c541c90c21434315b3
freshovich <81335287+freshovich@users.noreply.github.com>
2022-12-15 19:25:56 +0300

    Add files via upload

---
+++ CD2.4/data.js
@@ -0,0 +1,14 @@
+export default data = [
+  {
+    id: 0,
+    name: 'Jin',
+    image: require('./images/ex1.png'),
+    desc: 'Какая-то информация о персонаже',
+  },
+  {
+    id: 1,
+    name: 'Jaegger',
+    image: require('./images/example2.png'),
+    desc: 'Снова какая-то информация о персонаже. Но этот совершенно новый!',
+  },
+]
